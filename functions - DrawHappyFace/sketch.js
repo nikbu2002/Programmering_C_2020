1a197409c02f8c67fc5c92235511f46d250a7911
nikbu2002 <73332881+nikbu2002@users.noreply.github.com>
2020-11-06 10:38:39 +0100

    Finished Unit 5 Task
    Completed everything in the Unit 5 document.

--- functions - DrawHappyFace/sketch.js
+++ functions - DrawHappyFace/sketch.js
@@ -1,13 +1,41 @@
+var faceScale = 1;
+var faceType = 0;
+var faceRotation = 0;
+
 function setup() { 
 	createCanvas(600, 200);
-	noStroke();
+   noStroke();
 } 
 
 function draw() { 
-	background(131, 175, 155);
-   
-   translate(300, 100);
+   background(131, 175, 155);
 
+   drawHappyFace(300, 100, faceScale, faceType, faceRotation);
+
+   if(keyIsDown(68)){
+      faceRotation += 0.1;
+   }
+   
+   if(keyIsDown(65)){
+      faceRotation -= 0.1;
+   }
+
+   if(keyIsDown(87) && faceScale < 1.5){
+      faceScale += 0.02;
+   }
+
+   if(keyIsDown(83) && faceScale > 2/3){
+      faceScale -= 0.02;
+   }
+}
+
+function drawHappyFace(x, y, s, t, r){
+   push();
+   translate(x, y);
+   rotate(r);
+   scale(s);
+
+   if(t == 0){
    //Face
    fill(249,205,173);//rosy beige
    ellipse(0, 0, 100, 100);
@@ -21,11 +49,35 @@ function draw() {
 
    //Mouth
    fill(252,157,154);//light pink
-   arc(0, 25, 30, 30, 0, radians(180), PIE); 
-
+   arc(0, 25, 30, 30, 0, radians(180), PIE);
+   pop();
+   }
+   else if(t == 1){
+   //Face
+   fill(233,247,172);//greenish
+   ellipse(0, 0, 100, 100);
+   
+   //Eye 1
+   stroke(30);//dark gray
+   strokeWeight(2);
+   line(-5, 5, 5, 15);
+   line(-5, 15, 5, 5);
+   
+   //Eye 2
+   line(15, 5, 25, 15);
+   line(15, 15, 25, 5);
+ 
+   //Mouth
+   line(-10, 28, 10, 28);
+   pop();
+   }
 }
 
-function drawHappyFace(){
-	//Her kommer i selv til at skrive kode
+function keyPressed(){
+   if(keyCode == 69){
+      faceType = 1;
+   }
+   if(keyCode == 81){
+      faceType = 0;
+   }
 }
-
